ddbcaf9dd1105d930f60e59e13e669656dda1af9
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2023-08-08 17:20:47 +0700

    Init

---
+++ src/main/java/com/eltex/Main.java
@@ -0,0 +1,9 @@
+package com.eltex;
+
+public class Main {
+    public static void main(String[] args) {
+        final var service = new WallService();
+        service.like();
+        System.out.println(service.getPost()); // Post[content=, author=Anonymous, likes=1]
+    }
+}
