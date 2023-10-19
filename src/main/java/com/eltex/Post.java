0d2795084cc1cd17c502a816926c6297179d103e
Данил Кочубей <kochubeidanil@gmail.com>
2023-10-19 20:08:31 +0700

    Доработал Post по схеме

--- src/main/java/com/eltex/Post.java
+++ src/main/java/com/eltex/Post.java
@@ -1,23 +1,45 @@
 package com.eltex;
 
-public record Post(String content, String author, int likes) {
+public record Post(
+        long id,
+        long authorId,
+        String author,
+        String authorJob,
+        String authorAvatar,
+        String content,
+        String published,
+        String link,
+        boolean mentionedMe,
+        boolean likedByMe) {
 
     public Builder builder() {
         return new Builder()
                 .setAuthor(author)
-                .setContent(content)
-                .setLikes(likes);
+                .setContent(content);
     }
 
     static class Builder {
+
         // Обязательно указываем начальные значения полей
-        private String content = "";
+        private long id = 0;
+        private long authorId = 0;
         private String author = "";
-        private int likes = 0;
+        private String authorJob = "";
+        private String authorAvatar = "";
+        private String content = "";
+        private String published = "";
+        private String link = "";
+        private boolean mentionedMe = false;
+        private boolean likedByMe = false;
 
         // Каждый метод строителя запоминает данные и возвращает сам себя
-        public Builder setContent(final String content) {
-            this.content = content;
+        public Builder setId(final long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setAuthorId(final long authorId) {
+            this.authorId = authorId;
             return this;
         }
 
@@ -26,14 +48,54 @@ public record Post(String content, String author, int likes) {
             return this;
         }
 
-        public Builder setLikes(final int likes) {
-            this.likes = likes;
+        public Builder setAuthorJob(final String authorJob) {
+            this.authorJob = authorJob;
+            return this;
+        }
+
+        public Builder setAuthorAvatar(final String authorAvatar) {
+            this.authorAvatar = authorAvatar;
+            return this;
+        }
+
+        public Builder setContent(final String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder setPublished(final String published) {
+            this.published = published;
+            return this;
+        }
+
+        public Builder setLink(final String link) {
+            this.link = link;
+            return this;
+        }
+
+        public Builder setMentionedMe(final boolean mentionedMe) {
+            this.mentionedMe = mentionedMe;
+            return this;
+        }
+
+        public Builder setLikedByMe(final boolean likedByMe) {
+            this.likedByMe = likedByMe;
             return this;
         }
 
         // В финале вызываем build, чтобы получить результат
         public Post build() {
-            return new Post(content, author, likes);
+            return new Post(
+                    id,
+                    authorId,
+                    author,
+                    authorJob,
+                    authorAvatar,
+                    content,
+                    published,
+                    link,
+                    mentionedMe,
+                    likedByMe);
         }
     }
 }
--- src/main/java/com/eltex/WallService.java
+++ src/main/java/com/eltex/WallService.java
@@ -3,13 +3,6 @@ package com.eltex;
 public class WallService {
     private Post post = new Post.Builder().build();
 
-    public void like() {
-        final var likes = post.likes() + 1;
-        post = post.builder()
-                .setLikes(likes)
-                .build();
-    }
-
     public void setContent(final String content) {
         post = post.builder()
                 .setContent(content)
